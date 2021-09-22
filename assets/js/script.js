857aac9fecf332b5caffe1d1ee7a0bcf461467ef
Wintxer <47366828+Wintxer@users.noreply.github.com>
2021-09-22 11:38:04 +0200

    js paramètres

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,19 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q="+url.split("?ville=")[1
 
 
 function AfficheAPI(data) {
-    let div = document.querySelector(".meteo");
     if (data.message == "city not found") {
         div.insertAdjacentHTML("beforeend", 
         "<h2>La ville demandée n'a pas été trouvée</h2>");
     }
     else {        
-        div.insertAdjacentHTML("beforeend", 
-        "<h1>"+data.name+"</h1><img src='https://openweathermap.org/img/wn/"+data.weather[0].icon+".png' alt=''>");
+        document.querySelector(".weather_title").insertAdjacentHTML("beforeend", data.weather[0].description[0].toUpperCase()+data.weather[0].description.slice(1));
+        document.querySelector(".weather_icon").setAttribute("src", "https://openweathermap.org/img/wn/"+data.weather[0].icon+".png");
+        document.querySelector(".feels_like").insertAdjacentText("beforeend", "Ressenti "+Math.round(data.main.feels_like)+" °C");
+        document.querySelector(".wind").insertAdjacentText("afterbegin", (data.wind.speed*3.6).toFixed(2)+" km/h");
+        document.querySelector(".humidity").insertAdjacentText("afterbegin", data.main.humidity+" %");
+        document.querySelector(".weather_temp").insertAdjacentText("afterbegin", Math.round(data.main.temp)+" °");
+        document.querySelector(".weather_temp_min").insertAdjacentText("afterbegin", Math.round(data.main.temp_min)+" °C");
+        document.querySelector(".weather_temp_max").insertAdjacentText("afterbegin", Math.round(data.main.temp_max)+" °C");
     }
 }
+//<h1>"+data.name+"</h1><img src='https://openweathermap.org/img/wn/"+data.weather[0].icon+".png' alt=''>
