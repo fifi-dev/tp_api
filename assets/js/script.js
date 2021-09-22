248b6383be4c471d2c8917e06cffbe42b312cd1b
Wintxer <47366828+Wintxer@users.noreply.github.com>
2021-09-22 09:59:30 +0200

    affichage message erreur

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,14 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q="+url.split("?ville=")[1
     .then(function(data) {
         console.log(data);
         let div = document.querySelector(".meteo");
-        div.insertAdjacentHTML("beforeend", 
-        "<h1>"+data.name+"</h1><img src='https://openweathermap.org/img/wn/"+data.weather[0].icon+".png' alt=''>");
+        if (data.message == "city not found") {
+            div.insertAdjacentHTML("beforeend", 
+            "<h2>La ville demandée n'a pas été trouvée</h2>");
+        }
+        else {        
+            div.insertAdjacentHTML("beforeend", 
+            "<h1>"+data.name+"</h1><img src='https://openweathermap.org/img/wn/"+data.weather[0].icon+".png' alt=''>");
+        }
+
 
     })
